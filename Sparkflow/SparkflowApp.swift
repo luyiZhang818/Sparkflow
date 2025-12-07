0a5b7d7f52ea366445386837abb4751e2f7e5d6b
Luyi Zhang <iyulgnahz@outlook.com>
2025-12-06 20:12:03 -0500

    Initial Commit

---
+++ Sparkflow/SparkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkflowApp.swift
+//  Sparkflow
+//
+//  Created by Luyi Zhang on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SparkflowTests/SparkflowTests.swift
@@ -0,0 +1,17 @@
+//
+//  SparkflowTests.swift
+//  SparkflowTests
+//
+//  Created by Luyi Zhang on 12/6/25.
+//
+
+import Testing
+@testable import Sparkflow
+
+struct SparkflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
